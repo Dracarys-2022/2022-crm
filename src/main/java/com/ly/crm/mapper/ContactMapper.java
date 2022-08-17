40db59c5d5e7a165c7425fdb9362599c0a83f35b
刘洋 <1104282620@qq.com>
2022-08-17 16:32:45 +0800

    刘

---
+++ src/main/java/com/ly/crm/mapper/ContactMapper.java
@@ -0,0 +1,16 @@
+package com.ly.crm.mapper;
+
+import com.ly.crm.entity.Contact;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author ly
+ * @since 2022-07-17
+ */
+public interface ContactMapper extends BaseMapper<Contact> {
+
+}
---
+++ src/main/java/com/ly/crm/service/impl/LogsServiceImpl.java
@@ -0,0 +1,20 @@
+package com.ly.crm.service.impl;
+
+import com.ly.crm.entity.Logs;
+import com.ly.crm.mapper.LogsMapper;
+import com.ly.crm.service.LogsService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author ly
+ * @since 2022-07-17
+ */
+@Service
+public class LogsServiceImpl extends ServiceImpl<LogsMapper, Logs> implements LogsService {
+
+}
